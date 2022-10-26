abf2241e33afe086dbc2eb177c2307d366204247
tjdgns0029 <whtjdgns0029@naver.com>
2022-10-26 13:48:42 +0900

    다형성

---
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -0,0 +1,61 @@
+package chapter03;
+
+public class ArrayUtil {
+
+	public static double[] inToDouble(int[] a) {
+		double[] result = null;
+		
+		if(a == null) {
+			return result;
+		}
+		
+		result = new double[a.length];
+		for(int i = 0; i < a.length; i++) {
+			result[i] = a[i];
+		}
+		
+		return result;
+	}
+	
+	
+	public static int[] doubletoInt(double[] d) {
+		int[] result = null;
+		
+		if(d == null) {
+			return result;
+		}
+		
+		result = new int[d.length];
+		for(int i = 0; i < d.length; i++) {
+			result[i] = (int)d[i];
+		}
+		
+		return result;
+	}
+
+
+	public static int[] concat(int[] a, int[] b) {
+//		int size = 0;
+//		if(a != null) {
+//			size += a.length;
+//		}
+//		if (b != null) {
+//			size += b.length;
+//		}
+//		int[] result = new int[size];
+		
+		int[] result = new int[(a == null ? 0 : a.length) + (b == null ? 0 : b.length)];
+		
+		int index = 0;
+		for(int v : a) {
+			result[index++] = v;
+		}
+		
+		for(int v : b) {
+			result[index++] = v;	
+		}
+		
+		return result;
+	}
+
+}
