0a4f19566b3f05e9798b39c563df181d0e271345
tjdgns0029 <whtjdgns0029@naver.com>
2022-10-31 17:41:00 +0900

    review

--- practice01/src/main/java/prob3/review.java
+++ practice01/src/main/java/prob3/review.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 public class review {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("수를 입력하세요: ");
-		int s = scanner.nextInt();	
+		System.out.print("숫자를 입력하세요: ");
+		int num = scanner.nextInt();
+
+		int i = 0;
+		int sum = 0;
+
+		if (num % 2 == 1) {
+			for (i = 1; i <= num; i += 2) {
+				sum += i;
+			}
+		} else {
+			for (i = 2; i <= num; i += 2) {
+				sum += i;
+			}
+		}
+
+		System.out.println("결과값: " + sum);
 		scanner.close();
 	}
-	
 }
